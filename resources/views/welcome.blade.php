cfba5316cba27c30c4dc90c182d6e4942cd892fe
Rachel Loziuk <rachel.loziuk@gmail.com>
2017-04-10 15:50:51 -0500

    add master layout to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,28 +1,15 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Laravel</title>
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css">
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/core.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+@extends('layouts.app')
 
-  </head>
-  <body>
+@section('content')
     <div class="container">
-      <div id="login">
-        <a href="{{ url('/register') }}">Create Account</a>
-        <a href="{{ url('/login') }}">Log-In</a>
+
       </div>
       <div class="content">
         <div id="welcome-title">Fix My Ship</div>
       </div>
       <div id="get-started">
-        <!-- <button class="btn btn-info"><a href="/">Get Started!</a></button> -->
       
         <?php echo "This is PHP!"; ?>
       </div>
     </div>
-  </body>
-</html>
+@endsection
